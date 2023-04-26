67b7691c98bcb21bafea45062ff462e2c3dc52e7
anita <anasofialeo@gmail.com>
2023-04-26 08:55:44 -0500

    primera copia

---
+++ src/app/noticia/noticia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-noticia',
+  templateUrl: './noticia.component.html',
+  styleUrls: ['./noticia.component.css']
+})
+export class NoticiaComponent {
+
+}
+
---
+++ src/enviroments/enviroments.ts
@@ -0,0 +1,13 @@
+
+ export const firebaseConfig = {
+    production: false,
+    firebaseConfig:{
+    
+    apiKey: "AIzaSyAPjejqB2jbhfbqHX_UG4e6Kq179oLHnTQ",
+    authDomain: "anitaleonewsc.firebaseapp.com",
+    projectId: "anitaleonewsc",
+    storageBucket: "anitaleonewsc.appspot.com",
+    messagingSenderId: "634736697541",
+    appId: "1:634736697541:web:33ac251fd406d87d7510ac"
+}
+  };
